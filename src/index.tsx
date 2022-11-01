122ec678b6bb5bbee4c36e3412f771a221f416e5
YUNSIK.CHO <postop09@naver.com>
2022-11-01 17:22:43 +0900

    [Architecture] - TypeScript 로 프로젝트 툴 변경

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+/**
+ * createRoot 가 기대하는 type 과 getElementById 가 기대하는 type 이 일치하지 않는다.
+ * getElementById 는 null 을 반환할 수 있기 때문이다.
+ * getElementById 가 리턴하는 type 을 명시해줌으로 오류를 해결한다.
+ */
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
