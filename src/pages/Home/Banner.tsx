602a6cb86f192a7d5846b1500207e18d2e689b59
YUNSIK.CHO <postop09@naver.com>
2022-11-10 17:28:56 +0900

    #2 [홈]-배너-swiper.js 설치, ui frame 구축

---
+++ src/pages/Home/Banner.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+
+const Banner = () => {
+  return (
+    <section>
+      <HiddenTitle>배너</HiddenTitle>
+      <Swiper
+        modules={[Autoplay, Pagination]}
+        loop={true}
+        pagination={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <SlideImg src="" alt="광고를 신청해주세요!" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <SlideImg src="" alt="광고를 신청해주세요!" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <SlideImg src="" alt="광고를 신청해주세요!" />
+        </SwiperSlide>
+      </Swiper>
+    </section>
+  );
+};
+
+export default Banner;
+
+const SlideImg = styled.img`
+  min-height: 210px;
+`;
+
+const HiddenTitle = styled.h2`
+  ${({ theme }) => theme.TEXT.hide};
+`;
