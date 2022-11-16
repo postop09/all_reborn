cc68e3caff52c6eaf733347bc81651bd2132861c
YUNSIK.CHO <postop09@naver.com>
2022-11-16 14:27:01 +0900

    #14 [PWA] - 연결성공 - 라우터 주소 "/" 설정

--- src/index.tsx
+++ src/index.tsx
@@ -9,8 +9,6 @@ import Modal from "react-modal";
 import { AppProvider } from "./context/AppContext";
 import * as serviceWorker from "./serviceWorker";
 
-serviceWorker.register();
-
 if (process.env.NODE_ENV === "development") {
   const { worker } = require("./mocks/browser");
   worker.start();
@@ -22,7 +20,6 @@ Modal.setAppElement("#root");
  * getElementById 는 null 을 반환할 수 있기 때문이다.
  * getElementById 가 리턴하는 type 을 명시해줌으로 오류를 해결한다.
  */
-
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
@@ -34,3 +31,5 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>,
 );
+
+serviceWorker.register();
