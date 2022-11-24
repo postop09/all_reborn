8c9c7ec3941a8dc8674b5dd1a9b954883f3c2071
조윤식 <postop@tuneit.io>
2022-11-24 13:54:58 +0900

    #36 [지도] - 네이버지도 - 내 위치 찾기

--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -7,54 +7,40 @@ import MyModal from "../../components/modal/MyModal";
 import useModals from "../../hook/useModal";
 import styled from "styled-components";
 
+const { naver } = window;
+
 const Index = () => {
-  const [modal, setModal] = useState(false);
-  const { openModal } = useModals();
-
-  const onClick = () => {
-    console.log("OPEN!!");
-    setModal(true);
-  };
-
-  const onSubmit = () => {
-    console.log("SUBMIT!!");
-    const arrE = [];
-    console.log(!!arrE.length);
-    setModal(false);
-  };
-
-  const onClose = () => {
-    console.log("CLOSED!!");
-    setModal(false);
-  };
-
-  const FnModal = () => {
-    return <TestModal opened={modal} onClose={onClose} onSubmit={onSubmit} children={<>안녕하세요 모달입니다.</>} />;
-  };
-
-  const onClickContext = () => {
-    openModal(MyModal, {
-      onClose: () => {},
-    });
-  };
-
-  // NAVER MAP
-  const mapEl = useRef();
-
-  useEffect(() => {
-    const location = new naver.maps.LatLng(37.5656, 126.9769);
-    const mapOptions = {
-      center: new naver.maps.LatLng(37.3595704, 127.105399),
-      zoom: 10,
-    };
-
-    const map = new naver.maps.Map(mapEl.current, mapOptions);
-    new naver.maps.Marker({
-      position: location,
-      map,
-    });
-  }, []);
-
+  // MODAL
+  // const [modal, setModal] = useState(false);
+  // const { openModal } = useModals();
+  //
+  // const onClick = () => {
+  //   console.log("OPEN!!");
+  //   setModal(true);
+  // };
+  //
+  // const onSubmit = () => {
+  //   console.log("SUBMIT!!");
+  //   const arrE = [];
+  //   console.log(!!arrE.length);
+  //   setModal(false);
+  // };
+  //
+  // const onClose = () => {
+  //   console.log("CLOSED!!");
+  //   setModal(false);
+  // };
+  //
+  // const FnModal = () => {
+  //   return <TestModal opened={modal} onClose={onClose} onSubmit={onSubmit} children={<>안녕하세요 모달입니다.</>} />;
+  // };
+  //
+  // const onClickContext = () => {
+  //   openModal(MyModal, {
+  //     onClose: () => {},
+  //   });
+  // };
+  //
   // return (
   //   <>
   //     <OneWayButton text={"label12"} color={"keyWhite"} round={"md"} border={true} onClick={onClickContext}>
@@ -64,6 +50,61 @@ const Index = () => {
   //     {FnModal()}
   //   </>
   // );
+
+  // NAVER MAP
+  const mapEl = useRef<any>();
+  const [myLocation, setMyLocation] = useState<any>();
+
+  // 내 위치 찾기
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setMyLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      });
+    } else {
+      window.alert("현재위치를 알수 없습니다.");
+    }
+  }, []);
+
+  // useEffect(() => {
+  //   const location = new naver.maps.LatLng(37.5656, 126.9769);
+  //   new naver.maps.Marker({
+  //     position: location,
+  //     map,
+  //   });
+  // }, []);
+
+  // 내 위치 기준
+  useEffect(() => {
+    if (process.env.NODE_ENV !== "development") {
+      if (typeof myLocation !== "string") {
+        const location = new naver.maps.LatLng(myLocation.latitude, myLocation.longitude);
+        const mapOptions = {
+          center: location,
+          zoom: 14,
+        };
+        const map = new naver.maps.Map(mapEl.current, mapOptions);
+        new naver.maps.Marker({
+          position: location,
+          map,
+        });
+      }
+    } else {
+      const location = new naver.maps.LatLng(37.2755704, 127.042399);
+      const mapOptions = {
+        center: location,
+        zoom: 14,
+      };
+      const map = new naver.maps.Map(mapEl.current, mapOptions);
+      new naver.maps.Marker({
+        position: location,
+        map,
+      });
+    }
+  }, [myLocation]);
 
   return (
     <MapWrapper>
